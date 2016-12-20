5065ef119347c07cf64267b0d93500cc53e50832
unknown <jzabala@TIC-LP-007.dgcp.local>
2016-12-20 13:24:45 -0400

    Hot-reloading setup

--- client/components/App.js
+++ client/components/App.js
@@ -1,7 +1,11 @@
 import React from 'react';
 
-const App = () => (
-  <h1>Hello from react!</h1>
-);
+class App extends React.Component {
+  render() {
+    return (
+      <h1>Hello from react!</h1>
+    );
+  }
+}
 
 export default App;
