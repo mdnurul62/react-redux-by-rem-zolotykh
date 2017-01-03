393a8ed53fca3516d0184504805fa81d9a6396fd
Johnny Zabala <jzabala@dgcp.gov.do>
2017-01-03 15:19:58 -0400

    Fix bug in authentication middleware

--- server/middlewares/authenticate.js
+++ server/middlewares/authenticate.js
@@ -20,11 +20,11 @@ export default function authenticate(req, res, next) {
           select: [ 'username', 'id', 'email' ]
         }).fetch().then(user => {
           if(!user) {
-            res.statue(404).json({ error: 'No such user' });
+            res.status(404).json({ error: 'No such user' });
+          } else {
+            req.currentUser = user;
+            next();
           }
-
-          req.currentUser = user;
-          next();
         });
       }
     })
